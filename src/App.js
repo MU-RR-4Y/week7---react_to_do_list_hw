0b614c04a1a20f5a98731bca581edcba00094268
Michael Murray <mmurray24@hotmail.com>
2023-03-06 17:35:15 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [tasks,setTasks] = useState([
+    'Buy shopping', 
+    'Clean bathroom', 
+    "Car's MOT"
+  ]);
+
+  const taskNodes = tasks.map((task,index)=>{
+    return <li key={index}>{task}</li>
+  })
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>ToDo's</h1>
+    <form>
+      <input type="text" />
+      <label>High</label>
+      <input type="radio" name='priority' value='High'/>
+      <label>Low</label>
+      <input type="radio" name='priority' value='Low' />
+      <button >Save Item</button>
+    </form>
+    <ul>
+      {taskNodes}
+    </ul>
+    </>
   );
 }
 
