7f84e4e5ec10440efcec3ae978303835f551c815
Michael Murray <mmurray24@hotmail.com>
2023-03-06 21:28:56 +0000

    MVP completed

--- src/App.js
+++ src/App.js
@@ -9,25 +9,47 @@ function App() {
     'Clean bathroom', 
     "Car's MOT"
   ]);
+  // new variable to set temp list
+  const [newTask,setNewTask] = useState('')
 
+  //variable for list items in final return statement
   const taskNodes = tasks.map((task,index)=>{
     return <li key={index}>{task}</li>
   })
+  
+  //function retrieving value from updated form
+  function handleChange(event){
+    setNewTask(event.target.value)
+  }
 
+  //function to save a new list item
+  function saveTask (event) {
+    event.preventDefault();
+    console.log('new item:',newTask);
+    const newTasks =[...tasks, newTask];
+    setTasks(newTasks);
+    setNewTask('');
 
-
-
+    
+    
+  }
+  
+  
+  
   return (
     <>
     <h1>ToDo's</h1>
-    <form>
-      <input type="text" />
-      <label>High</label>
+
+    <form onSubmit={saveTask}>
+      <input type="text" onChange={handleChange}/>
+      {/* <label>High</label>
       <input type="radio" name='priority' value='High'/>
       <label>Low</label>
-      <input type="radio" name='priority' value='Low' />
+      <input type="radio" name='priority' value='Low' /> */}
       <button >Save Item</button>
     </form>
+
+
     <ul>
       {taskNodes}
     </ul>
